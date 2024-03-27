3cd8d494fb4e822485e684eebd0892fdfcc4c142
natnailg <natnailgebru@gmail.com>
2024-03-27 17:25:39 -0500

    reverting back to what i had.

--- testScanner.cpp
+++ testScanner.cpp
@@ -383,10 +383,10 @@ void L(){
         tokens = Scanner(); // consumes the fist of H ( for test 2 if i didn't consume it it was throwing an error);
 
         if(tokens.tokenid == (tokens.tokeninstance[0] == '?')) { //process ?
-            printf("K 1.2 instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
+            printf("L 1.2 instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
             tokens = Scanner(); //consumes ?
             L();
-            printf("K 1.2.1instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
+            printf("L 1.2.1instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
 // processing L
             return;
             printf("Hello\n");

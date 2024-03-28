f5b55dc58850596dde4e8bde09f9291b6dbfa550
natnailg <natnailgebru@gmail.com>
2024-03-27 19:03:54 -0500

    wokring on function L, H

--- testScanner.cpp
+++ testScanner.cpp
@@ -349,14 +349,14 @@ void L(){
         H(); //call H
 
         printf("L 1.1 instance %s tk id %s { ,; , . *\" } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
-        tokens = Scanner(); // consumes the fist of H ( for test 2 if i didn't consume it it was throwing an error);
+        //tokens = Scanner(); // consumes the fist of H ( for test 2 if i didn't consume it it was throwing an error);
         //printf("L 1.1 instance %s tk id %s after consuming ? { ,; , . *\" } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
 
         if(tokens.tokeninstance[0] == '?') { //process ?
             printf("L 1.2 instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
             tokens = Scanner(); //consumes ?
             L();
-            printf("L 1.2.1instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
+            printf("L 1.2.1 END instance %s tk id %s { ? } \n", tokens.tokeninstance, tokenNames[tokens.tokenid]);
 // processing L
             return;
             printf("Hello\n");

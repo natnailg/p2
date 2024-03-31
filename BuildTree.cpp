72e65579ee110133c1b282a2bb974e1fef4dc175
natnailg <natnailgebru@gmail.com>
2024-03-31 10:54:29 -0500

    it seems to work with test case 1,2,3 the only problem right now is printing the tokens and the indentations.

--- BuildTree.cpp
+++ BuildTree.cpp
@@ -58,6 +58,7 @@ void printAST(node_t* root, int depth) {
             printf("%*c %d, %s\n", depth * 3, ' ', root->token_id, root->token_instance);
 
         }else{
+            printf("inside of else %d", tokens.tokenid);
 
             printf("%*c %s\n", depth * 3, ' ', root->token_instance); // this is the only way which i can print the empty with out token being 0
 

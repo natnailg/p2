77020d2170b83e2d945ece1f34f833cd4554420f
natnailg <natnailgebru@gmail.com>
2024-03-30 21:14:19 -0500

    C was able to print. for the first if statement

--- BuildTree.cpp
+++ BuildTree.cpp
@@ -69,7 +69,7 @@ void printAST(node_t* root, int depth) {
     else{
         //printf("%c", root->Label);
         // Print token instance if it exists
-        printf(" (%d, %s)\n", root->token_id, root->token_instance);
+        printf("(%d, %s)\n", depth*5,root->token_id, root->token_instance);
 
     }
 
